92c37d5c1fcfad3cab554804d12a8665f9a203cc
Ken <ken.sk.lai@mail.foxconn.com>
2015-11-06 15:37:11 +0800

    2Workaround for add NCT driver

--- bin/startup_hacks.sh
+++ bin/startup_hacks.sh
@@ -2,12 +2,12 @@
 
 systemctl stop serial-getty@ttyS0
 
-echo 'module i2c_aspeed +p' > /sys/kernel/debug/dynamic_debug/control
-dmesg -c > /dev/null
-sleep 5
 echo nct7904 0x2e > /sys/bus/i2c/devices/i2c-6/new_device
-sleep 5
 echo nct7904 0x2d > /sys/bus/i2c/devices/i2c-6/new_device
 
-echo 'module i2c_aspeed -p' > /sys/kernel/debug/dynamic_debug/control
+echo 0x2d > /sys/bus/i2c/devices/i2c-6/delete_device
+echo 0x2e > /sys/bus/i2c/devices/i2c-6/delete_device
+
+echo nct7904 0x2e > /sys/bus/i2c/devices/i2c-6/new_device
+echo nct7904 0x2d > /sys/bus/i2c/devices/i2c-6/new_device
 
