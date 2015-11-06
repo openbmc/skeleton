b66f0646b970d266f0adab95ca930eb34657f4eb
Ken <ken.sk.lai@mail.foxconn.com>
2015-11-06 12:57:26 +0800

    Add -p debug message

--- bin/startup_hacks.sh
+++ bin/startup_hacks.sh
@@ -2,8 +2,12 @@
 
 systemctl stop serial-getty@ttyS0
 
+echo 'module i2c_aspeed +p' > /sys/kernel/debug/dynamic_debug/control
+
 sleep 5
 echo nct7904 0x2d > /sys/bus/i2c/devices/i2c-6/new_device
 sleep 5
 echo nct7904 0x2e > /sys/bus/i2c/devices/i2c-6/new_device
+
+echo 'module i2c_aspeed -p' > /sys/kernel/debug/dynamic_debug/control
 
