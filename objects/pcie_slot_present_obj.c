627961a91cc595a0b13593973b05888b56f001b5
Norman James <nkskjames@gmail.com>
2015-11-09 10:58:00 -0600

    update slot presents from barreleye

--- objects/pcie_slot_present_obj.c
+++ objects/pcie_slot_present_obj.c
@@ -5,16 +5,16 @@
 #include <string.h>
 #include "gpio.h"
 
-#define NUM_SLOTS 4
+#define NUM_SLOTS 8
 GPIO slots[NUM_SLOTS] = {
-//	{ "SLOT0_RISER_PRESENT" },
-//	{ "SLOT1_RISER_PRESENT" },
-//	{ "SLOT2_RISER_PRESENT" },
+	{ "SLOT0_RISER_PRESENT" },
+	{ "SLOT1_RISER_PRESENT" },
+	{ "SLOT2_RISER_PRESENT" },
 	{ "SLOT0_PRESENT" },
 	{ "SLOT1_PRESENT" },
 	{ "SLOT2_PRESENT" },
 	{ "MEZZ0_PRESENT" },
-//	{ "MEZZ1_PRESENT" },
+	{ "MEZZ1_PRESENT" },
 };
 
 typedef struct {
