ca3f6033a100f5134b7ea79ab3ece9846cde1d03
Norman James <njames@us.ibm.com>
2015-11-11 07:39:23 -0600

    add flash chip

--- objects/pflash/libflash/libflash.c
+++ objects/pflash/libflash/libflash.c
@@ -13,6 +13,10 @@ static const struct flash_info flash_info[] = {
                                 FL_ERASE_BULK | FL_MICRON_BUGS,
                                                           "Micron N25Qx512Ax"   },
 	{ 0x55aa55, 0x00100000, FL_ERASE_ALL | FL_CAN_4B, "TEST_FLASH" },
+	{ 0x20ba19, 0x02000000, FL_ERASE_4K  | FL_ERASE_64K | FL_CAN_4B |
+                                FL_ERASE_BULK | FL_MICRON_BUGS,
+                                                          "Micron N25Qx256Ax"   },
+
 };
 
 struct flash_chip {
