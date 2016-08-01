7282aeb4b256e5ad3016916a5f91202ce8e91749
Ratan Gupta <ratagupt@in.ibm.com>
2016-08-01 07:58:14 -0500

    Fixing WhiteSpace Error
    Change-Id: I81e847076deffbb191dc9db8503ae11dc555eb2d
    Signed-off-by: Ratan Gupta <ratagupt@in.ibm.com>

--- pystatemgr/discover_system_state.py
+++ pystatemgr/discover_system_state.py
@@ -41,36 +41,36 @@ dbus_objects = {
 }
 
 def getInterface(bus,objs,key):
-	obj = bus.get_object(objs[key]['bus_name'],objs[key]['object_name'],introspect=False)
-	return dbus.Interface(obj,objs[key]['interface_name'])
+    obj = bus.get_object(objs[key]['bus_name'],objs[key]['object_name'],introspect=False)
+    return dbus.Interface(obj,objs[key]['interface_name'])
 
 def getProperty(bus,objs,key,prop):
-	obj = bus.get_object(objs[key]['bus_name'],objs[key]['object_name'],introspect=False)
-	intf = dbus.Interface(obj,dbus.PROPERTIES_IFACE)
-	return intf.Get(objs[key]['interface_name'],prop)
+    obj = bus.get_object(objs[key]['bus_name'],objs[key]['object_name'],introspect=False)
+    intf = dbus.Interface(obj,dbus.PROPERTIES_IFACE)
+    return intf.Get(objs[key]['interface_name'],prop)
 
 
 bus = dbus.SystemBus()
 pgood = getProperty(bus,dbus_objects,'power','pgood')
 
 if (pgood == 1):
-	intf = getInterface(bus,dbus_objects,'bootprogress')
-	intf.setValue("FW Progress, Starting OS")
-	intf = getInterface(bus,dbus_objects,'occstatus0')
-	intf.setValue("Enabled")
-	intf = getInterface(bus,dbus_objects,'occstatus1')
-	intf.setValue("Enabled")
+    intf = getInterface(bus,dbus_objects,'bootprogress')
+    intf.setValue("FW Progress, Starting OS")
+    intf = getInterface(bus,dbus_objects,'occstatus0')
+    intf.setValue("Enabled")
+    intf = getInterface(bus,dbus_objects,'occstatus1')
+    intf.setValue("Enabled")
 else:
 	## Power is off, so check power policy
-	settings_intf = getInterface(bus,dbus_objects,'settings')
-	system_state = settings_intf.Get("org.openbmc.settings.Host","system_state")
-	power_policy = settings_intf.Get("org.openbmc.settings.Host","power_policy")
+    settings_intf = getInterface(bus,dbus_objects,'settings')
+    system_state = settings_intf.Get("org.openbmc.settings.Host","system_state")
+    power_policy = settings_intf.Get("org.openbmc.settings.Host","power_policy")
 
-	print "Last System State: "+system_state+";  Power Policy: "+power_policy
-	chassis_intf = getInterface(bus,dbus_objects,'chassis')
-	if (power_policy == "ALWAYS_POWER_ON" or
-	   (power_policy == "RESTORE_LAST_STATE" and 
-	    system_state =="HOST_POWERED_ON")):
-		chassis_intf.powerOn()
+    print "Last System State: "+system_state+";  Power Policy: "+power_policy
+    chassis_intf = getInterface(bus,dbus_objects,'chassis')
+    if (power_policy == "ALWAYS_POWER_ON" or
+       (power_policy == "RESTORE_LAST_STATE" and 
+        system_state =="HOST_POWERED_ON")):
+        chassis_intf.powerOn()
 
 
