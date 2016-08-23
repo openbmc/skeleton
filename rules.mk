caca713da6ea4544cac0d39f9c7a037c4e347f17
Brad Bishop <bradleyb@fuzziesquirrel.com>
2016-08-23 10:47:19 -0400

    pwrctl: add pgood state switching application
    pgood_wait simply blocks until the power state changes to
    the requested state.
    
    The intent is for use from a systemd unit similar to nm-online.
    
    Change-Id: Icf1e18f5d47d6eb2fc424b4614fd196ef3dfd6ee
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- rules.mk
+++ rules.mk
@@ -5,11 +5,15 @@ LDLIBS+=$(shell pkg-config --libs $(PACKAGE_DEPS))
 ALL_CFLAGS+=$(shell pkg-config --cflags $(PACKAGE_DEPS)) -fPIC -Werror $(CFLAGS)
 
 BIN_SUFFIX?=.exe
+.PHONY: $(SUBDIRS)
 
-all: $(BINS:=$(BIN_SUFFIX))
+all: $(BINS:=$(BIN_SUFFIX)) $(SUBDIRS)
 
 %.o: %.c
 	$(CC) -c $(ALL_CFLAGS) -o $@ $<
+
+$(SUBDIRS):
+	$(MAKE) -C $@
 
 $(BINS:=$(BIN_SUFFIX)): %$(BIN_SUFFIX): %.o $(EXTRA_OBJS)
 	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $^ $(LDLIBS)
@@ -19,6 +23,12 @@ install: $(BINS:=$(BIN_SUFFIX))
 	@for b in $(BINS:=$(BIN_SUFFIX)); do \
 		install $$b $(DESTDIR)$(sbindir) || exit 1; \
 	done
+	@for d in $(SUBDIRS); do \
+		$(MAKE) -C $$d $@ || exit 1; \
+	done
 
 clean:
 	rm -rf *.o $(BINS:=$(BIN_SUFFIX))
+	@for d in $(SUBDIRS); do \
+		$(MAKE) -C $$d $@ || exit 1; \
+	done
