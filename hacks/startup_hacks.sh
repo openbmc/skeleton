ce559683b52d0e1ee0fdaff678b37df05747415d
Timothy Pearson <tpearson@raptorengineering.com>
2016-09-18 13:41:10 -0500

    Enable fans on Firestone
    Change-Id: Id8d341322f19112d102c9df8a9e6bdb7e2ef633a
    Signed-off-by: Timothy Pearson <tpearson@raptorengineering.com>

--- hacks/startup_hacks.sh
+++ hacks/startup_hacks.sh
@@ -6,3 +6,12 @@ systemctl stop serial-getty@ttyS0
 VUART=/sys/devices/platform/ahb/ahb:apb/1e787000.vuart
 echo 4 > $VUART/sirq
 echo 0x3f8 > $VUART/lpc_address
+
+# Enable fans on Firestone
+FIRESTONE_FAN_CTL_BASE=/sys/class/i2c-adapter/i2c-3/3-0052/hwmon/hwmon0
+if [[ -e $FIRESTONE_FAN_CTL_BASE ]]; then
+	echo 1 > ${FIRESTONE_FAN_CTL_BASE}/pwm1_enable
+	echo 1 > ${FIRESTONE_FAN_CTL_BASE}/pwm2_enable
+	echo 1 > ${FIRESTONE_FAN_CTL_BASE}/pwm3_enable
+	echo 1 > ${FIRESTONE_FAN_CTL_BASE}/pwm4_enable
+fi
