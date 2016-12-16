openbmc/skeleton

commit 3ab5436a5c89ec5d89c0cb7e6497ddef7bc92555

op-pwrctl: Fix polarity of power outputs
The power GPIO logic introduced in
I83d55a7069558ec810bc2e0dbbff1c5495957e6d produces inverted polarity
outputs for power-up and reset signals. This fixes the logic and allows
systems to boot again.

Change-Id: I0830b46c0c0c1e337085050483da0250d5d850f5
Signed-off-by: Xo Wang <xow@google.com>

op-pwrctl/power_control_obj.c
@@ -88,7 +88,7 @@ poll_pgood(gpointer user_data)
 					continue;
 				}
 
-				reset_state = pgood_state ^ g_power_gpio.reset_pols[i];
+				reset_state = pgood_state ^ !g_power_gpio.reset_pols[i];
 				g_print("PowerControl: setting reset %s to %d\n", reset_out->name,
 						(int)reset_state);
 				gpio_write(reset_out, reset_state);
@@ -155,7 +155,7 @@ on_set_power_state(ControlPower *pwr,
 							g_power_gpio.power_up_outs[i].name, error);
 					continue;
 				}
-				power_up_out = state ^ g_power_gpio.power_up_pols[i];
+				power_up_out = state ^ !g_power_gpio.power_up_pols[i];
 				g_print("PowerControl: setting power up %s to %d\n",
 						g_power_gpio.power_up_outs[i].name, (int)power_up_out);
 				error = gpio_write(power_pin, power_up_out);

commit 3e14e213861c7cf6ce0280456f4cfc2b007a1831

Fix wrong reset pin setting
When power on or power off host, the way to set reset pins based on pin
polarity is not correct. This makes PCIE devices not working properly.

This patch fixed bug introduced by commit:
https://github.com/openbmc/skeleton/commit/3ab5436
and https://github.com/openbmc/skeleton/commit/0475f65

The relationship between expected reset pin state and pgood state looks like:

pgood polarilty reset_pin(expected)
1     0         1
0     0         0
1     1         0
0     1         1

Change-Id: Id0b36c15ed645c29f3ccc2aa507e50b31a718bb9
Signed-off-by: Yi Li <adamliyi@msn.com>

op-pwrctl/power_control_obj.c
@@ -91,9 +91,9 @@ poll_pgood(gpointer user_data)
 					continue;
 				}
 
-				reset_state = pgood_state ^ !g_power_gpio.reset_pols[i];
-				g_print("PowerControl: setting reset %s to %d\n", reset_out->name,
-						(int)reset_state);
+				reset_state = pgood_state ^ g_power_gpio.reset_pols[i];
+				g_print("PowerControl: pgood: %d, setting reset %s to %d\n",
+					(int)pgood_state, reset_out->name, (int)reset_state);
 				gpio_write(reset_out, reset_state);
 				gpio_close(reset_out);
 			}
@@ -121,9 +121,9 @@ poll_pgood(gpointer user_data)
 					continue;
 				}
 
-				reset_state = pgood_state ^ !g_power_gpio.pci_reset_pols[i];
-				g_print("PowerControl: setting pci reset %s to %d\n", pci_reset_out->name,
-						(int)reset_state);
+				reset_state = pgood_state ^ g_power_gpio.pci_reset_pols[i];
+				g_print("PowerControl: pgood: %d, setting pci reset %s to %d\n",
+					(int)pgood_state, pci_reset_out->name, (int)reset_state);
 				gpio_write(pci_reset_out, reset_state);
 				gpio_close(pci_reset_out);
 			}
@@ -203,9 +203,9 @@ on_boot_progress(GDBusConnection *connection,
 			continue;
 		}
 
-		reset_state = pgood_state ^ !g_power_gpio.pci_reset_pols[i];
-		g_print("PowerControl: setting pci reset %s to %d\n", pci_reset_out->name,
-				(int)reset_state);
+		reset_state = pgood_state ^ g_power_gpio.pci_reset_pols[i];
+		g_print("PowerControl: pgood: %d, setting pci reset %s to %d\n",
+			(int)pgood_state, pci_reset_out->name, (int)reset_state);
 		gpio_write(pci_reset_out, reset_state);
 		gpio_close(pci_reset_out);
 		g_print("Released pci reset: %s - %s\n", pci_reset_out->name, boot_progress);