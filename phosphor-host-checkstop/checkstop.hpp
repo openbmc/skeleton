66e332a2bcf04ebbc4ffb9a6737a3a2c33d8cc1e
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-03-13 00:44:02 +0530

    Checkstop: Add sd_event loop and callback handler
    Change-Id: I70b4c9b61afdf1535e28327805e2b0ec9c337ea2
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- phosphor-host-checkstop/checkstop.hpp
+++ phosphor-host-checkstop/checkstop.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <string>
-#include <iostream>
+#include <systemd/sd-event.h>
 namespace phosphor
 {
 namespace checkstop
@@ -14,7 +14,6 @@ class Handler
 {
     public:
         Handler() = delete;
-        ~Handler() = default;
         Handler(const Handler&) = delete;
         Handler& operator=(const Handler&) = delete;
         Handler(Handler&&) = delete;
@@ -22,20 +21,47 @@ class Handler
 
         /** @brief Constructs Handler object.
          *
-         *  @param[in] device   - Path to gpio device device
-         *  @param[in] line     - Line number in that gpio device
+         * @param[in] device   - Path to gpio device
+         * @param[in] line     - Line number in that gpio device
+         * @param[in] event    - sd_event handler
+         * @param[in] handler  - IO callback handler
          */
-        Handler(const std::string& device, uint32_t line)
+        Handler(const std::string& device, uint32_t line,
+                sd_event* event, sd_event_io_handler_t handler)
             : device(device),
-              line(line)
+              line(line),
+              event(event),
+              callbackHandler(handler)
+
         {
             // Get the file descriptor for the passed in device
             getFD();
+
+            // And register callback handler when FD has some data
+            registerCallback();
         }
 
+        ~Handler()
+        {
+            eventSource = sd_event_source_unref(eventSource);
+        }
+
+        /** Callback handler when the FD has some activity on it
+         *
+         * @param[in] es       - Populated event source
+         * @param[in] fd       - Associated File descriptor
+         * @param[in] revent   - Type of event
+         * @param[in] userData - User data that was passed during registration
+         *
+         * @return             - 0 or positive number on success and negative
+         *                       errno otherwise
+         */
+        static int processEvents(sd_event_source* es, int fd,
+                                 uint32_t revents, void* userData);
+
     private:
-        /** @brief Absolute path of GPIO device */
-        std::string device;
+        /** @brief Absolute path of device */
+        const std::string& device;
 
         /** @brief Line number in the device */
         uint32_t line;
@@ -43,8 +69,20 @@ class Handler
         /** @brief File descriptor for the gpio device */
         int fd = 0;
 
+        /** @brief Handler to sd_event */
+        sd_event *event;
+
+        /** @brief event source */
+        sd_event_source* eventSource = nullptr;
+
+        /** @brief Callback handler when the FD has some data */
+        sd_event_io_handler_t callbackHandler;
+
         /** @brief Opens the device and returns the descriptor */
         void getFD();
+
+        /** @brief attaches FD to events and sets up callback handler */
+        void registerCallback();
 };
 
 } // namespace checkstop
