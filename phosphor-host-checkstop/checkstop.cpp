749471dbdcb2d9df2b2a936f0ff0ef8b9a9bdc8d
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-03-12 17:51:55 +0530

    Checkstop: Get the file descriptor for the device
    On a GPIO chip, there could be many gpio lines.
    To know the assertion state of a GPIO line, a descriptor is needed
    for that line and the IOCTL that gets the FD is coming in 4.9 Kernel.
    With what we have now in 4.7, code is just getting the FD on the
    passed in file system entry and will be changed to do what is
    appropriate when 4.9 kernel is in.
    
    Change-Id: Icd28a234cbc453826aeac05fc3ccecb93bf2aadf
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

---
+++ phosphor-host-checkstop/checkstop.cpp
@@ -0,0 +1,43 @@
+/**
+ * Copyright © 2016 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <fcntl.h>
+#include <phosphor-logging/log.hpp>
+#include "checkstop.hpp"
+namespace phosphor
+{
+namespace checkstop
+{
+
+// Gets the file descriptor for passed in device
+void Handler::getFD()
+{
+    using namespace phosphor::logging;
+
+    // TODO: For now, this just opens the device and returns the FD.
+    // When 4.9 kernel is merged, this will be changed to use some IOCTL to
+    // associate a FD to passed in GPIO device.
+    fd = open(device.c_str(), O_RDONLY | O_NONBLOCK);
+    if (fd < 0)
+    {
+        log<level::ERR>("Failed to open device",
+                entry("PATH=%s", device.c_str()));
+        throw std::runtime_error("Failed to open device");
+    }
+}
+
+} // namespace checkstop
+} // namespace phosphor
