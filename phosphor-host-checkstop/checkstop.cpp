93f12b007c30a8404cfcc99b1980599d6a4868b8
Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>
2017-03-22 13:44:32 +0530

    Checkstop: Call host quiesce target when GPIO line is asserted
    Fixes openbmc/openbmc#766
    
    Change-Id: Ie739f9098d943c26ad4f3df274e15e994a0e7ed1
    Signed-off-by: Vishwanatha Subbanna <vishwa@linux.vnet.ibm.com>

--- phosphor-host-checkstop/checkstop.cpp
+++ phosphor-host-checkstop/checkstop.cpp
@@ -16,11 +16,27 @@
 
 #include <fcntl.h>
 #include <phosphor-logging/log.hpp>
+#include <xyz/openbmc_project/State/Host/server.hpp>
 #include "checkstop.hpp"
 namespace phosphor
 {
 namespace checkstop
 {
+
+// systemd service to kick start a target.
+constexpr auto SYSTEMD_SERVICE        = "org.freedesktop.systemd1";
+constexpr auto SYSTEMD_ROOT           = "/org/freedesktop/systemd1";
+constexpr auto SYSTEMD_INTERFACE      = "org.freedesktop.systemd1.Manager";
+constexpr auto HOST_QUIESCE_TARGET    = "obmc-quiesce-host@0.target";
+
+// To know the current state of Host.
+constexpr auto HOST_STATE_MANAGER_ROOT  = "/xyz/openbmc_project/state/host0";
+constexpr auto HOST_STATE_MANAGER_IFACE = "xyz.openbmc_project.State.Host";
+constexpr auto DBUS_PROPERTY_IFACE      = "org.freedesktop.DBus.Properties";
+constexpr auto PROPERTY                 = "CurrentHostState";
+
+// Phosphor State manager
+namespace State = sdbusplus::xyz::openbmc_project::State::server;
 
 using namespace phosphor::logging;
 
@@ -56,9 +72,103 @@ void Handler::registerCallback()
 int Handler::processEvents(sd_event_source* es, int fd,
                            uint32_t revents, void* userData)
 {
-    // TODO. This calls into starting quiesce target
-    log<level::INFO>("Callback handler called");
+    log<level::INFO>("GPIO line altered");
+    auto handler = static_cast<Handler*>(userData);
+    return handler->analyzeEvent();
+}
+
+// Analyzes the GPIO event
+int Handler::analyzeEvent()
+{
+    // To extract the current state of Host
+    sdbusplus::message::variant<std::string> currHostState {};
+
+    // Who is hosting the State Manager service.
+    auto service = getServiceName(HOST_STATE_MANAGER_ROOT,
+                                  HOST_STATE_MANAGER_IFACE);
+    if(service.empty())
+    {
+        // Do not want to get into quiesce state when we don't know for sure.
+        return 0;
+    }
+
+    auto method = bus.new_method_call(service.c_str(),
+                                      HOST_STATE_MANAGER_ROOT,
+                                      DBUS_PROPERTY_IFACE,
+                                      "Get");
+    method.append(HOST_STATE_MANAGER_IFACE);
+    method.append(PROPERTY);
+
+    // If the last known HOST state is Running, then transition
+    // to Quiesce state.
+    auto reply = bus.call(method);
+    reply.read(currHostState);
+
+    // TODO Check if the GPIO really asserted.
+    if (currHostState == State::convertForMessage(
+                State::Host::HostState::Running))
+    {
+        auto method = bus.new_method_call(SYSTEMD_SERVICE,
+                                          SYSTEMD_ROOT,
+                                          SYSTEMD_INTERFACE,
+                                          "StartUnit");
+        method.append(HOST_QUIESCE_TARGET);
+        method.append("replace");
+
+        // If there is any error, an exception would be thrown from here.
+        bus.call_noreply(method);
+
+        // This marks the completion of handling the checkstop and app can exit
+        completed = true;
+    }
+    else
+    {
+        log<level::INFO>("Host is not Running. ignoring GPIO assertion",
+                entry("CURRENT_STATE=%s", currHostState));
+    }
     return 0;
+}
+
+// Given the LED dbus path and interface, returns the service name
+std::string Handler::getServiceName(const std::string& objPath,
+                                    const std::string& interface)
+{
+    using namespace phosphor::logging;
+
+    // Mapper dbus constructs
+    constexpr auto MAPPER_BUSNAME   = "xyz.openbmc_project.ObjectMapper";
+    constexpr auto MAPPER_OBJ_PATH  = "/xyz/openbmc_project/ObjectMapper";
+    constexpr auto MAPPER_IFACE     = "xyz.openbmc_project.ObjectMapper";
+
+    // Make a mapper call
+    auto mapperCall = bus.new_method_call(MAPPER_BUSNAME, MAPPER_OBJ_PATH,
+                                          MAPPER_IFACE, "GetObject");
+    // Cook rest of the things.
+    mapperCall.append(objPath);
+    mapperCall.append(std::vector<std::string>({interface}));
+
+    auto reply = bus.call(mapperCall);
+    if (reply.is_method_error())
+    {
+        // Its okay if we do not see a corresponding physical LED.
+        log<level::INFO>("Error looking up service",
+                entry("PATH=%s",objPath.c_str()));
+        return "";
+    }
+
+    // Response by mapper in the case of success
+    std::map<std::string, std::vector<std::string>> serviceNames;
+
+    // This is the service name for the passed in objpath
+    reply.read(serviceNames);
+    if (serviceNames.empty())
+    {
+        log<level::INFO>("Service lookup did not return any service",
+                entry("PATH=%s",objPath.c_str()));
+        return "";
+    }
+
+    return serviceNames.begin()->first;
 }
 
 } // namespace checkstop
