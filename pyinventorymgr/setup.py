f9559b917fa3badd7aff9829201651c9f0ed464f
Josh D. King <jdking@us.ibm.com>
2017-04-18 12:34:48 -0500

    Clean old inventory / remove inventory_items
    Removed inventory_items.py
    Removed inventory_items in setup
    
    Change-Id: I8d4ff51e131c402b0d600c41c73b69f1bbc9dadf
    Signed-off-by: Josh D. King <jdking@us.ibm.com>

--- pyinventorymgr/setup.py
+++ pyinventorymgr/setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(name='pyinventorymgr',
       version='1.0',
       packages=['obmc.inventory'],
-      scripts=['inventory_items.py', 'sync_inventory_items.py'],
+      scripts=['sync_inventory_items.py'],
       )
