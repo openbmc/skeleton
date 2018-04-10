7f90ba26fd741881183630c24f4ffcfcbfae54ab
Patrick Venture <venture@google.com>
2018-04-10 13:02:09 -0700

    example of unit-tests using sdbusplus:: mocks
    Examples of message::append and message::read as well as
    calls into sdbusplus::bus::bus
    
    Change-Id: Icda9e55d6d5e164fe8b65cd67f6009652a21ae84
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ utexample/mac.cpp
@@ -0,0 +1,67 @@
+#include "mac.h"
+
+#include <cstdio>
+#include <iostream>
+#include <sdbusplus/bus.hpp>
+
+constexpr auto MAC_FORMAT = "%02hhx:%02hhx:%02hhx:%02hhx:%02hhx:%02hhx";
+constexpr auto MAC_INTERFACE = "xyz.openbmc_project.Network.MACAddress";
+constexpr auto NETWORK_INTERFACE = "xyz.openbmc_project.Network";
+constexpr auto PROP_INTERFACE = "org.freedesktop.DBus.Properties";
+constexpr auto IFACE_ROOT = "/xyz/openbmc_project/network/";
+
+// Not yet tested.
+bool ParseMac(const std::string& mac_addr, MacAddr* mac)
+{
+  int ret = std::sscanf(mac_addr.c_str(), MAC_FORMAT,
+                     mac->octet,
+                     mac->octet + 1,
+                     mac->octet + 2,
+                     mac->octet + 3,
+                     mac->octet + 4,
+                     mac->octet + 5);
+  return (ret == 6);
+}
+
+// Now fully unit-tested.
+bool GetMacAddr(
+  MacAddr* mac,
+  const std::string& iface_path,
+  sdbusplus::bus::bus &dbus)
+{
+  if (mac == nullptr)
+  {
+    std::cerr << "GetMacAddr MAC Address is nullptr" << std::endl;
+    return false;
+  }
+
+  auto networkd_call = dbus.new_method_call(
+                             NETWORK_INTERFACE,
+                             iface_path.c_str(),
+                             PROP_INTERFACE,
+                             "Get");
+
+  // this calls the tuple with ss
+  networkd_call.append(MAC_INTERFACE, "MACAddress");
+
+  auto reply = dbus.call(networkd_call);
+  if (reply.is_method_error())
+  {
+    std::cerr << "GetMacAddr Failed to get MAC Address" << std::endl;
+    return false;
+  }
+
+  sdbusplus::message::variant<std::string> result;
+  reply.read(result);
+
+  auto mac_addr = result.get<std::string>();
+  if (!ParseMac(mac_addr, mac))
+  {
+    std::cerr << "GetMacAddr Failed to parse MAC Address: "
+              << mac_addr
+              << std::endl;
+    return false;
+  }
+
+  return true;
+}
