e8c92a3cb79f2045bf53e84dec642a35f86a7378
Adriana Kobylak <anoo@us.ibm.com>
2018-08-08 11:26:18 -0500

    configs: Remove the OperatingSystemStatus sensor
    The Operating System Status sensor has moved from
    /org/openbmc/sensors to /xyz/openbmc_project/state/host0.
    Remove the sensor from the org path.
    
    Tested: Powered on a witherspoon to the Host OS.
    
    Change-Id: I02d50d0a38485833fd73579c5aec41fd157141d0
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- configs/Zaius.py
+++ configs/Zaius.py
@@ -369,7 +369,6 @@ GPIO_CONFIGS = {
 # The sensor id is the same as those defined in ID_LOOKUP['SENSOR'].
 MISC_SENSORS = {
     0x07 : { 'class' : 'BootCountSensor' },
-    0x02 : { 'class' : 'OperatingSystemStatusSensor' },
 }
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
