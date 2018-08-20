d7a8ba5b581fde659881ea9f67e3526ab50ca50a
Adriana Kobylak <anoo@us.ibm.com>
2018-08-20 12:01:59 -0500

    configs: Remove TurboAllowedSensor
    The TurboAllowed sensor is already mapped to
    /xyz/openbmc_project/control/host0/turbo_allowed
    (Reference: the phosphor-ipmi-sensor-inventory-mrw-config/config.yaml file).
    Remove it from the deprecated /org/openbmc/sensors path.
    
    Tested: Powered on a Witherspoon to the Host OS without errors.
    
    Change-Id: I3232f66e338f986a21df8aae5401c97b565cbdd6
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- configs/Witherspoon.py
+++ configs/Witherspoon.py
@@ -302,7 +302,6 @@ ID_LOOKUP = {
 MISC_SENSORS = {
     # Garrison value is used, Not in P9 XML yet.
     0x0b : { 'class' : 'PowerSupplyRedundancySensor'},
-    0xda : { 'class' : 'TurboAllowedSensor' },
     0xD8 : { 'class' : 'PowerSupplyDeratingSensor' },
 }
 
