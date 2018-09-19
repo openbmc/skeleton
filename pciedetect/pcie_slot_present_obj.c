898bebc4f60e4442418666a33509c48e2aac32ed
Adriana Kobylak <anoo@us.ibm.com>
2018-09-19 12:40:52 -0500

    pciedetect: Remove reference to system_manager
    The GPIOs have moved out of D-Bus (see openbmc/openbmc#3332),
    specifically out of system_manager, so there's no need to
    query that object anymore to check if the GPIOs exist, the
    call to read them would indicate a failure if not found.
    
    Tested: The current code as is doesn't work, it fails in the
    get_service() call at least on Witherpsoon and Zaius.
    With this change at least we get to try to read the GPIOs
    which don't currently exist in the JSON file, so if this app
    was needed all it requires is to add the GPIOs to the json file:
    Sep 19 18:00:57 witherspoon pcie_slot_present.exe[1486]: Unable to find GPIO SLOT0_RISER_PRESENT in the JSON
    
    Change-Id: Id3803f8478e8bc60e4889f4e454ee25d78c5fbc4
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- pciedetect/pcie_slot_present_obj.c
+++ pciedetect/pcie_slot_present_obj.c
@@ -26,87 +26,6 @@ typedef struct {
 
 
 /* ------------------------------------------------------------------------- */
-void
-get_service(GDBusConnection *connection, const char *obj,
-		const char **service, GError **error)
-{
-	GDBusProxy *proxy;
-	GVariant *result = NULL;
-	GVariantIter *iter;
-
-	*error = NULL;
-	proxy = g_dbus_proxy_new_sync(connection,
-			G_DBUS_PROXY_FLAGS_NONE,
-			NULL, /* GDBusInterfaceInfo* */
-			"xyz.openbmc_project.ObjectMapper", /* name */
-			"/xyz/openbmc_project/object_mapper", /* object path */
-			"xyz.openbmc_project.ObjectMapper", /* interface name */
-			NULL, /* GCancellable */
-			error);
-
-	result = g_dbus_proxy_call_sync(proxy,
-			"GetObject",
-			g_variant_new("(s)", obj),
-			G_DBUS_CALL_FLAGS_NONE,
-			-1,
-			NULL,
-			error);
-	if(*error)
-		goto exit;
-
-	g_variant_get(result, "(a{sas})", &iter);
-	g_variant_iter_next(iter, "{sas}", service, NULL);
-
-exit:
-	if(result)
-		g_variant_unref(result);
-}
-
-int
-get_object(GDBusConnection *connection, GDBusProxy *proxy,
-		GPIO* gpio, object_info* obj_info)
-{
-	g_print("Checking Presence: %s\n",gpio->name);
-	const char *gpio_bus = NULL;
-	GError *error;
-	GVariant *parm;
-	GVariant *result;
-	int rc=0;
-
-	error = NULL;
-	parm = g_variant_new("(ss)","GPIO_PRESENT",gpio->name);
-	result = g_dbus_proxy_call_sync(proxy,
-			"getObjectFromId",
-			parm,
-			G_DBUS_CALL_FLAGS_NONE,
-			-1,
-			NULL,
-			&error);
-	g_assert_no_error(error);
-	if(error)
-		goto exit;
-
-	GVariantIter *iter = g_variant_iter_new(result);
-	GVariant* v_result = g_variant_iter_next_value(iter);
-
-	g_variant_get(v_result,"(ss)",&obj_info->path,&obj_info->intf_name);
-
-	get_service(connection, obj_info->path, &gpio_bus, &error);
-	if(error)
-		goto exit;
-
-	obj_info->bus_name = gpio_bus;
-
-exit:
-	if(!gpio_bus || strlen(gpio_bus) == 0) {
-		rc = 1;
-	}
-	g_variant_unref(v_result);
-	g_variant_unref(result);
-
-	return rc;
-}
-
 int
 get_presence(GPIO* gpio, uint8_t* present)
 {
@@ -126,7 +45,6 @@ get_presence(GPIO* gpio, uint8_t* present)
 	{
 		printf("ERROR pcie_slot_present: GPIO error %s (rc=%d)\n",gpio->name,rc);
 	}
-	gpio_inits_done();
 	return rc;
 }
 
@@ -165,33 +83,14 @@ update_fru_obj(GDBusConnection* connection, object_info* obj_info, const char* p
 gint
 main(gint argc, gchar *argv[])
 {
-	const char *sysmgr_path = "/org/openbmc/managers/System";
-	const char *sysmgr_bus = NULL;
 	GMainLoop *loop;
 	GDBusConnection *c;
-	GDBusProxy *sys_proxy;
 	GError *error;
 
 	loop = g_main_loop_new(NULL, FALSE);
 
 	error = NULL;
 	c = g_bus_get_sync(DBUS_TYPE, NULL, &error);
-	if(error)
-		goto exit;
-
-	get_service(c, sysmgr_path, &sysmgr_bus, &error);
-	if(error)
-		goto exit;
-
-	sys_proxy = g_dbus_proxy_new_sync(c,
-			G_DBUS_PROXY_FLAGS_NONE,
-			NULL, /* GDBusInterfaceInfo* */
-			sysmgr_bus, /* name */
-			sysmgr_path, /* object path */
-			"org.openbmc.managers.System", /* interface name */
-			NULL, /* GCancellable */
-			&error);
-	g_assert_no_error(error);
 	if(error)
 		goto exit;
 
@@ -202,10 +101,8 @@ main(gint argc, gchar *argv[])
 		object_info obj_info;
 		uint8_t present;
 		do {
-			rc = get_object(c,sys_proxy,&slots[i],&obj_info);
-			if(rc) { break; }
 			rc = get_presence(&slots[i],&present);
-			//if (rc) { break; }
+			if (rc) { break; }
 			// TODO: send correct state
 			if(present == 0) {
 				update_fru_obj(c,&obj_info,"True");
@@ -216,8 +113,6 @@ main(gint argc, gchar *argv[])
 	}
 
 exit:
-	if(sysmgr_bus)
-		g_free((char *)sysmgr_bus);
 	g_object_unref(c);
 	g_main_loop_unref(loop);
 	return 0;
